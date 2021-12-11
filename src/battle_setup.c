e200c4d6789eb449672477a9c3b380bc78370843
Mina Kemp <aliceminacarter@gmail.com>
2021-12-11 00:13:38 -0500

    yup

--- src/battle_setup.c
+++ src/battle_setup.c
@@ -941,9 +941,11 @@ static void CB2_GiveStarter(void)
     starterMon = GetStarterPokemon(gSpecialVar_Result);
     ScriptGiveMon(starterMon, 5, ITEM_NONE, 0, 0, 0);
     ResetTasks();
-    PlayBattleBGM();
-    SetMainCallback2(CB2_StartFirstBattle);
-    BattleTransition_Start(B_TRANSITION_BLUR);
+    SetMainCallback2(CB2_ReturnToFieldContinueScript);
+    FadeScreen(FADE_TO_BLACK, 0);
+    // PlayBattleBGM();
+    // SetMainCallback2(CB2_StartFirstBattle);
+    // BattleTransition_Start(B_TRANSITION_BLUR);
 }
 
 static void CB2_StartFirstBattle(void)
--- src/field_control_avatar.c
+++ src/field_control_avatar.c
@@ -580,7 +580,7 @@ static bool8 TryStartStepCountScript(u16 metatileBehavior)
         }
         if (ShouldDoScottBattleFrontierCall() == TRUE)
         {
-            ScriptContext1_SetupScript(LittlerootTown_ProfessorBirchsLab_EventScript_ScottAboardSSTidalCall);
+            // ScriptContext1_SetupScript(LittlerootTown_ProfessorBirchsLab_EventScript_ScottAboardSSTidalCall);
             return TRUE;
         }
         if (ShouldDoRoxanneCall() == TRUE)
